71c9c681a7f91bb78b50402da5a8b0f9f66b5262
muhamad jaelani <jaelanim465@gmail.com>
2026-03-05 14:04:47 +0700

    adding cors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::{Extension, Router};
 use dotenvy::dotenv;
 use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
 
 mod config;
 mod handlers;
@@ -16,10 +17,16 @@ async fn main() {
 
     let db = config::database::connect().await;
 
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .merge(routes::auth_routes::auth_routes())
         .merge(routes::user_routes::user_routes())
-        .layer(Extension(db));
+        .layer(Extension(db))
+        .layer(cors);
 
     let port = std::env::var("APP_PORT")
         .ok()
